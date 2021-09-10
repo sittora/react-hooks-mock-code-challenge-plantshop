58cba4836217b95e8e6b46c61da3fe4519b4f076
sittora <sittora@gmail.com>
2021-09-10 08:17:52 -0400

    still working on this

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,36 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, onDeletePlant, onUpdatePlant }) {
+  // const [plants, setPlants] = useState([])
+
+
+  // useEffect(() => {
+  //   fetch(`http://localhost:6001/plants`)
+  //     .then(response => response.json())
+  //     .then(plantsArray => {
+  //       setPlants(plantsArray)
+  //     })
+  // }, [])
+
+
+  //  plants.map ((plantObject) => (
+  //    <PlantCard key={plantObject.id} name={plantObject.name} image={plantObject.image} price={plantObject.price}/>;
+  // })
+
+
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map ((plant) => {
+     <PlantCard key={plant.id} plant={plant} onDeletePlant={onDeletePlant} onUpdatePlant={onUpdatePlant}/>;
+      
+  })}
+      </ul>
   );
 }
 
 export default PlantList;
+
+
+
